e067c99982caa3eab8890d71ec6279667565e782
skt-t1-byungi <tiniwz@gmail.com>
2019-01-22 11:29:22 +0900

    fix dts

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,13 @@ export declare class Container<State extends {}>{
     protected readonly state: State
     public setState(updater: Partial<State> | ((prevState: State) => Partial<State> | void)): Promise<void>
     public on(render: (state:State) => React.ReactNode): React.ReactElement<{render: typeof render}>
-    public on<Selected>( selector: (state:State) => Selected, render: (state:Selected) => React.ReactNode): React.ReactElement<{render: typeof render}>
+    public on<Selected>(
+        selector: (state:State) => Selected, render: (state:Selected) => React.ReactNode
+    ): React.ReactElement<{render: typeof render}>
 }
 
 type Selector<Containers, Result> = (state:
     Containers extends [Container<infer R1>] ? [R1]
-    : Containers extends [Container<infer R1>, Container<infer R2>] ? [R1, R2]
     : Containers extends [Container<infer R1>, Container<infer R2>] ? [R1, R2]
     : Containers extends [Container<infer R1>, Container<infer R2>, Container<infer R3>] ? [R1, R2, R3]
     : Containers extends [Container<infer R1>, Container<infer R2>, Container<infer R3>, Container<infer R4>] ? [R1, R2, R3, R4]
